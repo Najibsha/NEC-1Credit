4a678d3748d51aed66cf386c9280f5ba5573097f
Najibsha <najibsherode07@gmail.com>
2025-03-09 08:07:41 +0530

    ml-model.py

--- ml-model.py
+++ ml-model.py
@@ -9,19 +9,17 @@ iris = datasets.load_iris()
 X = iris.data
 y = iris.target
 
-# Split dataset into training set and test set
+# Split dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-# Create a Gaussian Classifier
+# Create a classifier
 clf = RandomForestClassifier()
 
-# Train the model using the training sets
+# Train the model
 clf.fit(X_train, y_train)
 
-# Predict the response for test dataset
+# Predict and evaluate
 y_pred = clf.predict(X_test)
-
-# Model Accuracy, how often is the classifier correct?
 print(f"Accuracy: {accuracy_score(y_test, y_pred)}")
 
 # Save the trained model
